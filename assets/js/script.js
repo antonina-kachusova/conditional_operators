cb3c6836fc3382b67525a2c7b8598f177a643e80
antonina-kachusova <antoninakachusova@gmail.com>
2024-07-09 23:33:02 +0300

    Loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,81 +1,29 @@
-// If-Else statement
-// Write a script that checks whether the number written in the initial variable is even. 
-// The program should output "Even number" or "Odd number" to the console, depending on the number 
-// which is assigned to the initial variable.
+// Loop While
 
-let value = 7;
-
-if (value % 2 === 0) {
-    console.log('The number is even ' + value);
-}
-else {
-    console.log('The number is odd ' + value);
-}
-
-// Switch operator
-// Create a program that, for the numbers 0 to 6, outputs the name of the day of the week, 
-// corresponding to this number (0 - Monday, 1 - Tuesday, etc.). 
-// If the number is not in this range, the program should output "Invalid number".
-
-let week = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ];
-  
-  let dayNumber = 7;
-  
-  if (
-      !Number.isInteger(dayNumber) 
-      || dayNumber < 0 
-      || dayNumber > 6
-  ) {
-    console.log("Very Wrong number or Out of range");
-  } else {
-    switch (dayNumber) {
-      case 0:
-        console.log(`${dayNumber} - ${week[dayNumber]}`);
-        break;
-      case 1:
-        console.log(`${dayNumber} - ${week[dayNumber]}`);
-        break;
-      case 2:
-        console.log(`${dayNumber} - ${week[dayNumber]}`);
-        break;
-      case 3:
-        console.log(`${dayNumber} - ${week[dayNumber]}`);
-        break;
-      case 4:
-        console.log(`${dayNumber} - ${week[dayNumber]}`);
-        break;
-      case 5:
-        console.log(`${dayNumber} - ${week[dayNumber]}`);
-        break;
-      case 6:
-        console.log(`${dayNumber} - ${week[dayNumber]}`);
-        break;
-      default:
-        console.log("Wrong number");
-        break;
-    }
+let n = 33;
+while (n >= 23) {
+  if (n % 3 === 0) {
+    console.log(n);
   }
+  n--;
+}
 
-// Ternary operator
-// Write code that uses the ternary operator to determine the larger of two numbers 
-// (create two initial variables). 
-// The program should output "The first number is larger", "The first number is larger" or "The numbers are equal", 
-// depending on the numbers assigned to the initial variables. 
-// a ternary operator can be added as one of the values ​​of another ternary operator
+// Loop Do/While
 
-let smallNum = 8;
-let bigNum = 8;
+let count = 0;
+let randomNumber = Math.floor(Math.random() * 10) + 1;
 
-let message1 = 'The first number is larger';
-let message2 = 'The second number is larger';
-let message3 = 'The numbers are equal';
+do {
+    console.log('number ' + randomNumber);
+    count++;
+  
+} while (randomNumber !== 8 && count < 1000); 
 
-bigNum > smallNum ? console.log(message1) : (bigNum < smallNum? console.log(message2) : console.log(message3));
+console.log(`The number ${randomNumber} was generated after ${count} iterations.`);
+   
+// Loop For
+
+for (let i = 2; i < 11; i++) {
+  let value = 2 ** i;
+  console.log(`2 to the power of ${i} is ${value}`);
+}
